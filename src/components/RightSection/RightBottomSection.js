8f705b4536b3e453d133a17de3d4eb2fe9ea9f79
AJean144 <ajeanjacques42@gmail.com>
2019-08-02 13:16:59 -0400

    Display liked gifs to right section

--- src/components/RightSection/RightBottomSection.js
+++ src/components/RightSection/RightBottomSection.js
@@ -7,42 +7,64 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { connect } from 'react-redux';
 
-const cards = [1, 2, 3, 4];
-
-const RightBottomSection = () => {
+const RightBottomSection = ({ likedGifs }) => {
   const classes = useStyles();
+
+  const getHowManyLeft = (gifsLength) => {
+    const amountLeft = 5 - gifsLength;
+    const pluralOrSingular = amountLeft === 1 ? '' : 's';
+    return (
+      <Typography className={classes.youMustMessage}>
+        You must <span className={classes.italic}>Like</span> {amountLeft} more GIF{pluralOrSingular} to calculate your score
+      </Typography>
+    )
+  }
+
+  let allLikedGifsDisplay = [];
+  for (let key in likedGifs) {
+    allLikedGifsDisplay.unshift(
+      <Grid item key={likedGifs[key].id} xs={12} sm={6} md={6}>
+        <Card className={classes.card}>
+          <CardMedia
+            className={classes.cardMedia}
+            image={likedGifs[key].id ? likedGifs[key].images.downsized.url : ''}
+            title={likedGifs[key].title ? likedGifs[key].title : 'Search for a GIF'}
+          />
+          <CardActions className={classes.cardButton}>
+            <Button variant="contained" color="primary">
+              Unlike
+        </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    )
+  }
 
   return (
     <>
       <Container className={classes.cardGrid} maxWidth="md">
         <Grid container direction="row" justify="center" alignItems="center" spacing={4}>
-          {cards.map(card => (
-            <Grid item key={card} xs={12} sm={6} md={6}>
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  image=""
-                  title="GIF title"
-                />
-                <CardActions className={classes.cardButton}>
-                  <Button variant="contained" color="primary">
-                    Unlike
-                </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
+          {allLikedGifsDisplay}
         </Grid>
       </Container>
       <Grid container direction="row" justify="center" alignItems="center">
         <Button variant="contained" color="primary">
           Calculate My Weirdness Score
       </Button>
-        <Typography className={classes.youMustMessage}>You must <span className={classes.italic}>Like</span> 1 more GIF to calculate your score</Typography>
+        {getHowManyLeft(allLikedGifsDisplay.length)}
       </Grid>
     </>
   )
 }
 
-export default RightBottomSection;
+const mapStateToProps = state => {
+  const { likedGifs } = state;
+
+  return {
+    likedGifs: likedGifs.likedGifs
+  }
+}
+
+export default connect(mapStateToProps)(RightBottomSection);
